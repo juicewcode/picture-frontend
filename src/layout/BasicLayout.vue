fbc080908fbcdf5730e1f9cc47cf7d142230c4e7
nxbhx <491852417@qq.com>
2024-12-21 02:58:41 +0800

    前端第一期，搭建了页面框架

---
+++ src/layout/BasicLayout.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="basicLayout">
+        <a-layout style="min-height: 100vh;">
+            <a-layout-header class="header">
+                <GlobelHeader/>
+            </a-layout-header>
+            <a-layout-content class="content">
+                <!-- <router-view/> -->
+            </a-layout-content>
+            <a-layout-footer class="footer">
+                <a href="http://www.juicew.com" target="_blank">果汁云图库</a>
+            </a-layout-footer>
+        </a-layout>
+    </div>
+</template>
+
+<script setup lang="ts">
+import GlobelHeader from '@/components/GlobelHeader.vue';
+
+
+</script>
+
+<style scoped>
+
+#basicLayout .header{
+    padding-inline: 20px;
+    /* margin-bottom: 16px; */
+    color: unset;
+    background: white;
+}
+
+#basicLayout .content{
+    padding: 20px;
+    background: linear-gradient(to right, #fefefe, #fff);
+    /* margin-bottom: 28px; */
+}
+
+#basicLayout .footer{
+    background: #efefef;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    text-align: center;
+    padding: 16px;
+
+}
+</style>
+
+
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="homePage">
+
+  </div>
+</template>
+
+
+
+
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
