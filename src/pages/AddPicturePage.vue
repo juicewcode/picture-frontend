eaec870b6b34bb54b2dd47ef19f7a25dbb326cba
nxbhx <491852417@qq.com>
2024-12-29 01:09:49 +0800

    完成抓取图片，用户上传图片，图片审核的开发

--- src/pages/AddPicturePage.vue
+++ src/pages/AddPicturePage.vue
@@ -3,7 +3,17 @@
   <h2 style="margin-bottom: 16px">  
   {{ route.query?.id ? '修改图片' : '创建图片' }}  
   </h2>
-  <PictureUpload :picture="picture" :onSuccess="onSuccess" />  
+  <!-- 选择上传方式 -->  
+  <a-tabs v-model:activeKey="uploadType"  
+    >>  
+    <a-tab-pane key="file" tab="文件上传">  
+      <PictureUpload :picture="picture" :onSuccess="onSuccess" />  
+    </a-tab-pane>  
+    <a-tab-pane key="url" tab="URL 上传" force-render>  
+      <UrlPictureUpload :picture="picture" :onSuccess="onSuccess" />  
+    </a-tab-pane>  
+  </a-tabs>
+
   <a-form v-if="picture" layout="vertical" :model="pictureForm"@finish="handleSubmit">  
     <a-form-item label="名称" name="name">  
       <a-input v-model:value="pictureForm.name" placeholder="请输入名称" />  
@@ -53,11 +63,11 @@ import { useRouter } from 'vue-router'
 import { onMounted } from 'vue'
 import { getPictureVoByIdUsingGet } from '@/api/pictureController'
 import { useRoute } from 'vue-router'
+import UrlPictureUpload from '../components/UrlPictureUpload.vue'
 
 
 
-
-
+const uploadType = ref<'file' | 'url'>('file')
 const picture = ref<API.PictureVO>()  
 const pictureForm = reactive<API.PictureEditRequest>({})
 
--- src/pages/admin/PictureManagePage.vue
+++ src/pages/admin/PictureManagePage.vue
@@ -2,10 +2,13 @@
     <div id="pictureManagerPage">
         <!-- // 搜索 -->
          <a-flex justify="space-between">  
-            <h2>图片管理</h2>  
+          <h2>图片管理</h2>  
+           <a-space>  
             <a-button type="primary" href="/add_picture" target="_blank">+ 创建图片</a-button>  
+            <a-button type="primary" href="/add_picture/batch" target="_blank" ghost>+ 批量创建图片</a-button>  
+          </a-space>
         </a-flex>
-
+        <div style="margin-bottom: 16px;"></div>
         <a-form layout="inline" :model="searchParams" @finish="doSearch">  
             <a-form-item label="关键词" name="searchText">  
                 <a-input  
@@ -26,6 +29,15 @@
                 allow-clear  
                 />  
             </a-form-item>  
+            <a-form-item label="审核状态" name="reviewStatus">  
+              <a-select  
+                v-model:value="searchParams.reviewStatus"  
+                :options="PIC_REVIEW_STATUS_OPTIONS"  
+                placeholder="请输入审核状态"  
+                style="min-width: 180px"  
+                allow-clear  
+              />  
+            </a-form-item>
             <a-form-item>  
                 <a-button type="primary" html-type="submit">搜索</a-button>  
             </a-form-item>  
@@ -54,18 +66,42 @@
                     <div>宽高比：{{ record.picScale }}</div>  
                     <div>大小：{{ (record.picSize / 1024).toFixed(2) }}KB</div>  
                 </template>  
+                <!-- 审核信息 -->  
+                <template v-if="column.dataIndex === 'reviewMessage'">  
+                  <div>审核状态：{{ PIC_REVIEW_STATUS_MAP[record.reviewStatus] }}</div>  
+                  <div>审核信息：{{ record.reviewMessage }}</div>  
+                  <div>审核人：{{ record.reviewerId }}</div>  
+                </template>
                 <template v-else-if="column.dataIndex === 'createTime'">  
                     {{ dayjs(record.createTime).format('YYYY-MM-DD HH:mm:ss') }}  
                 </template>  
                 <template v-else-if="column.dataIndex === 'editTime'">  
                     {{ dayjs(record.editTime).format('YYYY-MM-DD HH:mm:ss') }}  
                 </template>  
-                <template v-else-if="column.key === 'action'">                     
-                    <a-space>  
-                        <a-button type="link" :href="`/add_picture?id=${record.id}`" target="_blank">编辑</a-button>  
-                        <a-button type="link" danger @click="doDelete(record.id)">删除</a-button>  
-                    </a-space>
-                </template>  
+                <template v-else-if="column.key === 'action'">  
+                  <a-space wrap>  
+                    <a-button  
+                      v-if="record.reviewStatus !== PIC_REVIEW_STATUS_ENUM.PASS"  
+                      type="link"  
+                      @click="handleReview(record, PIC_REVIEW_STATUS_ENUM.PASS)"  
+                    >  
+                      通过  
+                    </a-button>  
+                    <a-button  
+                      v-if="record.reviewStatus !== PIC_REVIEW_STATUS_ENUM.REJECT"  
+                      type="link"  
+                      danger  
+                      @click="handleReview(record, PIC_REVIEW_STATUS_ENUM.REJECT)"  
+                    >  
+                      拒绝  
+                    </a-button>  
+                    <a-button type="link" :href="`/add_picture?id=${record.id}`" target="_blank"  
+                      >编辑  
+                    </a-button>  
+                    <a-button type="link" danger @click="doDelete(record.id)">删除</a-button>  
+                  </a-space>  
+                </template>
+
             </template>
         </a-table>
     </div>
@@ -74,7 +110,7 @@
 
 
 <script lang="ts" setup>
-import { deletePictureUsingPost, listPictureVoByPageUsingPost } from '@/api/pictureController';
+import { deletePictureUsingPost, doPictureReviewUsingPost, listPictureVoByPageUsingPost } from '@/api/pictureController';
 import { SmileOutlined, DownOutlined } from '@ant-design/icons-vue';
 import { message } from 'ant-design-vue';
 import { computed, onMounted, reactive, ref } from 'vue';
@@ -84,6 +120,9 @@ import { useRouter } from 'vue-router';
 import { useLoginUserStore } from '@/stores/useLoginUserStore';
 import { useRoute } from 'vue-router';
 import { listPictureByPageUsingPost } from '@/api/pictureController';
+import { PIC_REVIEW_STATUS_MAP } from '@/constants/picture';
+import { PIC_REVIEW_STATUS_ENUM } from '@/constants/picture';
+import { PIC_REVIEW_STATUS_OPTIONS } from '@/constants/picture';
 
 const columns = [  
   {  
@@ -129,6 +168,10 @@ const columns = [
     title: '编辑时间',  
     dataIndex: 'editTime',  
   },  
+  {
+    title: '审核信息',
+    dataIndex: 'reviewMessage',
+  },
   {  
     title: '操作',  
     key: 'action',  
@@ -209,6 +252,21 @@ const doDelete = async (id: string) => {
   }
 }
 
+const handleReview = async (record: API.Picture, reviewStatus: number) => {  
+  const reviewMessage = reviewStatus === PIC_REVIEW_STATUS_ENUM.PASS ? '管理员操作通过' : '管理员操作拒绝'  
+  const res = await doPictureReviewUsingPost({  
+    id: record.id,  
+    reviewStatus,  
+    reviewMessage,  
+  })  
+  if (res.data.code === 0) {  
+    message.success('审核操作成功')  
+    // 重新获取列表  
+    fetchData()  
+  } else {  
+    message.error('审核操作失败，' + res.data.message)  
+  }  
+}
 
 
 </script>
