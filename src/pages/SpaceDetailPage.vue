740f11c08f9f6c34f7a8af3d8b41a4019fe9b8a8
nxbhx <491852417@qq.com>
2025-01-11 18:49:36 +0800

    图库数据分析功能

--- src/pages/SpaceDetailPage.vue
+++ src/pages/SpaceDetailPage.vue
@@ -7,6 +7,16 @@
                 <a-button type="primary" :href="`/add_picture?spaceId=${id}`" target="_blank">
                 + 创建图片
                 </a-button>
+                  <a-button
+                  type="primary"
+                  ghost
+                  :icon="h(BarChartOutlined)"
+                  :href="`/space_analyze?spaceId=${id}`"
+                  target="_blank"
+                  >
+                  空间分析
+                </a-button>
+
                 <a-button :icon="h(EditOutlined)" @click="doBatchEdit"> 批量编辑</a-button>
 
 
@@ -78,7 +88,7 @@ import PictureSearchForm from '@/components/PictureSearchForm.vue';
 import { ColorPicker } from 'vue3-colorpicker';
 import "vue3-colorpicker/style.css";
 import BatchEditPictureModal from '@/components/BatchEditPictureModal.vue';
-import { EditOutlined } from '@ant-design/icons-vue';
+import { EditOutlined,BarChartOutlined } from '@ant-design/icons-vue';
 import { h } from 'vue';
 
 // 分享弹窗引用
